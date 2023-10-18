f62462a464c1ab44548f6989c906523197fc84be
Kartik Saxena <karrtiksaxena@gmail.com>
2023-10-18 16:17:14 +0530

    modules

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("hello bhai")
+const math = require("./math.js") // ./ means current directory aur agar nhi lagauya toh woh apni 
+//inbuilt modeules ko hi lega
+//use destructuring also like : const{add,sub} = require("./math")
+
+console.log("hello bhai");
+
+console.log(math.add(5,3))
+
+console.log(math.sub(5,3))
--- math.js
+++ math.js
@@ -37,3 +37,15 @@ like “start” : node hello.js
 
 */
 
+function add(a,b){
+
+return a + b ;
+
+}
+function sub(a,b){
+
+    return a - b ;
+    
+    }
+
+module.exports= {add, sub} ;

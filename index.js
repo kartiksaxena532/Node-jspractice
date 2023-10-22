6803b5f86744d78ca0ce997f29cd356158156398
Kartik Saxena <karrtiksaxena@gmail.com>
2023-10-23 01:24:16 +0530

    full server made

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
 
-    const log= `${Date.now()}:New Request At this time\n`;
+    const log= `${Date.now()}:${req.url} New Request At this time\n`; // req.url path dega ki request kahan kahan pe hui
     fs.appendFile("log.txt",log,(err,data)=>{
-        
-        res.end("hello from server again");
+        switch(req.ur){ //applying multi routes on req.url
+            case "/": //home page
+                res.end("hello from server again");
+                break;
+            case "/about":
+                res.end("About Page");
+                break;
+            case "/contact":
+                res.end("Contact Page");
+                break;
+            default:
+                res.end("404 not found");
+        }
+        ;
     });
     //console.log(req); //req.Headers can also be used
    // res.end("hello sab sahi haina")

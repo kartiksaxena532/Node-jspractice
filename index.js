b9bd611551571c80a6ce9ce3c34a7c40ac0d9be3
Kartik Saxena <karrtiksaxena@gmail.com>
2023-10-24 01:29:52 +0530

    full web server updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const http = require("http");
+const http = require("http"); // doesnt parse res.url hence cannot diffrenciate between path and query parameters.
 const fs = require("fs");
-
+const url = require("url"); // import package for url parsing
 const myServer = http.createServer((req,res)=>{
-
+    if (req.url === "/favicon.ico") return  res.end(); // this would generate an empty response for favicon
+    
     const log= `${Date.now()}:${req.url} New Request At this time\n`; // req.url path dega ki request kahan kahan pe hui
+    const myUrl = url.parse(req.url,true);//true means i want to use parsequery parameters
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.ur){ //applying multi routes on req.url
+        switch(myUrl.pathname){ //applying multi routes on req.url i.e path name
             case "/": //home page
                 res.end("hello from server again");
                 break;
@@ -17,8 +20,7 @@ const myServer = http.createServer((req,res)=>{
                 break;
             default:
                 res.end("404 not found");
-        }
-        ;
+        };
     });
     //console.log(req); //req.Headers can also be used
    // res.end("hello sab sahi haina")

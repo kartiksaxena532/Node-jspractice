db8b9fc0dbeaf7a77c98de72fc6da86afebe8d84
Kartik Saxena <karrtiksaxena@gmail.com>
2023-10-26 23:08:03 +0530

    express 1

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const http = require("http"); // doesnt parse res.url hence cannot diffrenciate between path and query parameters.
 const fs = require("fs");
 const url = require("url"); // import package for url parsing
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{   
+    return res.end("this is home page");
+});
+
+app.get('/about',(req,res)=>{   
+    return res.end("this is about page");
+});
+
+
+
+
 const myServer = http.createServer((req,res)=>{
     if (req.url === "/favicon.ico") return  res.end(); // this would generate an empty response for favicon
     

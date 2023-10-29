e5c7e3d1a39a585a9e201c03f974b440f1274c70
Kartik Saxena <karrtiksaxena@gmail.com>
2023-10-29 17:10:45 +0530

    rest api ssr and csr

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-
+const data = require("./MOCK_DATA.json");
 const app = express();
 
-const port = 8000;
+const PORT = 8000;
+
+app.get("/data",(req,res)=>{
+    const html =`
+    <ul>
+    ${data.map((data)=>`<li>${data.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+        });
+
+app.get("/api/data",(req,res)=>{
+
+return res.json(data);
+
+});
+
+app.get("/api/data/:id",(req,res)=>{
+
+    const id = Number(req.params.id);
+    const person = data.find((person)=>person.id === id);
+    return res.json(person);
+});
+
 
 //routes
 
-app.listen(port,()=>console.log(`heyy server has been started at ${port}`));
+app.listen(PORT,()=>console.log(`heyy server has been started at ${PORT}`));

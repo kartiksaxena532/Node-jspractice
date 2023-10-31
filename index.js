5ff84cdd76242dc3f6c089b18f27a3731a05ea31
Kartik Saxena <karrtiksaxena@gmail.com>
2023-11-01 01:20:08 +0530

    Get by id postrequest

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
 const express = require('express');
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 
 const PORT = 8000;
 
 app.use(express.urlencoded({extended:false})); //like a plugin "MIDDLEWARE"
+// increasing usability for the route path if changed in future.
 
-
-app
-.route("/api/users")// increasing usability for the route path if changed in future.
-.get((req,res)=>{
-
+app.get("/api/users/:id",(req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user)=>user.id === id);
     return res.json(user);
 })
 
-.post((req,res)=>{
+app.post("/api/users",(req,res)=>{
     const body = req.body;
-    console.log("Body",body) //read notion docs
-    return res.json({status : "pending"});
-})
-.patch((req,res)=>{
-  
-    return res.json({status : "pending"});
-})
-.delete((req,res)=>{
-  
-    return res.json({status : "pending"});
-})
-.put((req,res)=>{
-  
-    return res.json({status : "pending"});
-})
+    ///console.log("Body",body) //read notion docs
+    users.push({...body,id: users.length +1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
+        return res.json({status : "success" , id :users.length});
+    })
+    ;
+});
 //routes
 
 app.listen(PORT,()=>console.log(`heyy server has been started at ${PORT}`));

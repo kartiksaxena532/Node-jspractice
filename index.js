737cb152b5e4d1f5b3194c04cc366f3fd2527621
Kartik Saxena <karrtiksaxena@gmail.com>
2023-11-01 00:42:31 +0530

    postman testing

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ const app = express();
 
 const PORT = 8000;
 
-app.get("/data",(req,res)=>{
-    const html =`
-    <ul>
-    ${data.map((data)=>`<li>${data.first_name}</li>`).join("")}
-    </ul>
-    `;
-    res.send(html);
-        });
 
-app.get("/api/data",(req,res)=>{
 
-return res.json(data);
-
-});
 // increasing usability for the route path if changed in future.
 
-
-
-app.get("/api/data/:id",(req,res)=>{
+app
+.route("/api/data/:id")
+.get((req,res)=>{
 
     const id = Number(req.params.id);
     const person = data.find((person)=>person.id === id);
     return res.json(person);
-});
+})
 
-
+.post((req,res)=>{
+  
+    return res.json({status : "pending"});
+})
+.patch((req,res)=>{
+  
+    return res.json({status : "pending"});
+})
+.delete((req,res)=>{
+  
+    return res.json({status : "pending"});
+})
+.put((req,res)=>{
+  
+    return res.json({status : "pending"});
+})
 //routes
 
 app.listen(PORT,()=>console.log(`heyy server has been started at ${PORT}`));
